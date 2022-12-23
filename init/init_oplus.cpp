3529487b6840024c9a2f2f6dc5344e25fc3b68d0
Andrea055 <vivanco11.ac@gmail.com>
2022-12-23 09:01:04 +0100

    sm8350: init oplus -> realme

--- init/init_oplus.cpp
+++ init/init_oplus.cpp
@@ -33,46 +33,15 @@ void OverrideProperty(const char* name, const char* value) {
  * after the original property has been set.
  */
 void vendor_load_properties() {
-    auto device = GetProperty("ro.product.product.device", "");
-    auto rf_version = std::stoi(GetProperty("ro.boot.rf_version", "0"));
+    auto prjname = std::stoi(GetProperty("ro.boot.prjname", "0"));
 
-    switch (rf_version) {
-        case 11: // CN
-            if (device == "OnePlus9") {
-                OverrideProperty("ro.product.product.model", "LE2110");
-            } else if (device == "OnePlus9Pro") {
-                OverrideProperty("ro.product.product.model", "LE2120");
-            }
-            break;
-        case 12: // TMO
-            if (device == "OnePlus9") {
-                OverrideProperty("ro.product.product.model", "LE2117");
-            } else if (device == "OnePlus9Pro") {
-                OverrideProperty("ro.product.product.model", "LE2127");
-            }
-            break;
-        case 13: // IN
-            if (device == "OnePlus9") {
-                OverrideProperty("ro.product.product.model", "LE2111");
-            } else if (device == "OnePlus9Pro") {
-                OverrideProperty("ro.product.product.model", "LE2121");
-            }
-            break;
-        case 21: // EU
-            if (device == "OnePlus9") {
-                OverrideProperty("ro.product.product.model", "LE2113");
-            } else if (device == "OnePlus9Pro") {
-                OverrideProperty("ro.product.product.model", "LE2123");
-            }
-            break;
-        case 22: // NA
-            if (device == "OnePlus9") {
-                OverrideProperty("ro.product.product.model", "LE2115");
-            } else if (device == "OnePlus9Pro") {
-                OverrideProperty("ro.product.product.model", "LE2125");
-            }
+    switch (prjname) {
+        case 216B2: // RMX3312
+            OverrideProperty("ro.product.product.device", "RE58B2L1");
+            OverrideProperty("ro.product.product.model", "RMX3312");
+            OverrideProperty("ro.product.product.name", "RMX3312EEA");
             break;
         default:
-            LOG(ERROR) << "Unexpected RF version: " << rf_version;
+            LOG(ERROR) << "Unexpected project version: " << prjname;
     }
 }
